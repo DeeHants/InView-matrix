73533aa93ae49b5131051188a84f68b7cb7e5e7f
Deanna Earley <dee@earlsoft.co.uk>
2025-11-08 13:12:02 +0000

    Move the debug loop into a new function

--- main.cpp
+++ main.cpp
@@ -12,6 +12,50 @@ int main() {
     printf("Initializing pins...\n");
     init_pins();
 
+    // Enable this to enter debug loop (never exits)
+    // debug_loop();
+
+    while (true) {
+        printf("LED Matrix Controller Running...\n");
+        sleep_ms(1000);
+    }
+}
+
+void init_pins() {
+    // All pins are output, pulled high and set to 0 for a truthy state
+    gpio_init(PIN_STROBE);
+    gpio_set_dir(PIN_STROBE, true);
+    gpio_pull_down(PIN_STROBE);
+    gpio_put(PIN_STROBE, false); // Start low (enabled)
+
+    gpio_init(PIN_OUTPUT);
+    gpio_set_dir(PIN_OUTPUT, true);
+    gpio_pull_down(PIN_OUTPUT);
+    gpio_put(PIN_OUTPUT, false); // Start low (enabled)
+
+    gpio_init(PIN_CLOCK_Y);
+    gpio_set_dir(PIN_CLOCK_Y, true);
+    gpio_pull_down(PIN_CLOCK_Y);
+    gpio_put(PIN_CLOCK_Y, true);
+
+    gpio_init(PIN_DATA_Y);
+    gpio_set_dir(PIN_DATA_Y, true);
+    gpio_pull_down(PIN_DATA_Y);
+    gpio_put(PIN_DATA_Y, true);
+
+    gpio_init(PIN_CLOCK_R);
+    gpio_set_dir(PIN_CLOCK_R, true);
+    gpio_pull_down(PIN_CLOCK_R);
+    gpio_put(PIN_CLOCK_R, true);
+
+    gpio_init(PIN_DATA_R);
+    gpio_set_dir(PIN_DATA_R, true);
+    gpio_pull_down(PIN_DATA_R);
+    gpio_put(PIN_DATA_R, true);
+}
+
+// Enters a debug loop allowing manual control of GPIO pins via STDIN.
+void debug_loop() {
     while (true) {
         printf(
             "strobe=%c output=%c clk_y=%c data_y=%c clk_r=%c data_r=%c\n",
@@ -93,37 +137,4 @@ int main() {
 
         sleep_ms(10);
     }
-}
-
-void init_pins() {
-    // All pins are output, pulled high and set to 0 for a truthy state
-    gpio_init(PIN_STROBE);
-    gpio_set_dir(PIN_STROBE, true);
-    gpio_pull_down(PIN_STROBE);
-    gpio_put(PIN_STROBE, false); // Start low (enabled)
-
-    gpio_init(PIN_OUTPUT);
-    gpio_set_dir(PIN_OUTPUT, true);
-    gpio_pull_down(PIN_OUTPUT);
-    gpio_put(PIN_OUTPUT, false); // Start low (enabled)
-
-    gpio_init(PIN_CLOCK_Y);
-    gpio_set_dir(PIN_CLOCK_Y, true);
-    gpio_pull_down(PIN_CLOCK_Y);
-    gpio_put(PIN_CLOCK_Y, true);
-
-    gpio_init(PIN_DATA_Y);
-    gpio_set_dir(PIN_DATA_Y, true);
-    gpio_pull_down(PIN_DATA_Y);
-    gpio_put(PIN_DATA_Y, true);
-
-    gpio_init(PIN_CLOCK_R);
-    gpio_set_dir(PIN_CLOCK_R, true);
-    gpio_pull_down(PIN_CLOCK_R);
-    gpio_put(PIN_CLOCK_R, true);
-
-    gpio_init(PIN_DATA_R);
-    gpio_set_dir(PIN_DATA_R, true);
-    gpio_pull_down(PIN_DATA_R);
-    gpio_put(PIN_DATA_R, true);
 }
