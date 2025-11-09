08615801b7d6bde0c2c5617360c4861c95488f9e
Deanna Earley <dee@earlsoft.co.uk>
2025-11-09 23:22:24 +0000

    Add a PIO program to feed the red/yellow click and data lines

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 #include "pico/stdlib.h"
 
 #include "hardware/gpio.h"
+#include "hardware/pio.h"
 
 #include "main.h"
 #include "pins.h"
 #include "debug.h"
+#include "data_line.pio.h"
+
+PIO pio;
+uint dl_yellow;
+uint dl_red;
 
 int main() {
     stdio_init_all();
@@ -13,11 +19,19 @@ int main() {
     printf("Initializing pins...\n");
     init_pins();
 
+    init_data_line_pio();
+
     // Enable this to enter debug loop (never exits)
     // debug_loop();
 
     while (true) {
         printf("LED Matrix Controller Running...\n");
+        pio_sm_put_blocking(pio, dl_yellow, 0x21);
+        pio_sm_put_blocking(pio, dl_yellow, 0x43);
+        pio_sm_put_blocking(pio, dl_yellow, 0x65);
+        pio_sm_put_blocking(pio, dl_yellow, 0x87);
+        pio_sm_put_blocking(pio, dl_yellow, 0xa9);
+
         debug_step();
     }
 }
@@ -53,4 +67,13 @@ void init_pins() {
     gpio_set_dir(PIN_DATA_R, true);
     gpio_pull_down(PIN_DATA_R);
     gpio_put(PIN_DATA_R, true);
+}
+
+void init_data_line_pio() {
+    pio = pio0;
+    uint offset = pio_add_program(pio, &data_line_program);
+    dl_yellow = pio_claim_unused_sm(pio, true);
+    data_line_program_init(pio, dl_yellow, offset, PIN_DATA_Y, PIN_CLOCK_Y, 1.0f);
+    dl_red = pio_claim_unused_sm(pio, true);
+    data_line_program_init(pio, dl_red, offset, PIN_DATA_R, PIN_CLOCK_R, 1.0f);
 }
--- main.h
+++ main.h
@@ -9,6 +9,8 @@ extern "C" {
 // This function performs hardware setup and does not return a value.
 void init_pins(void);
 
+void init_data_line_pio();
+
 #ifdef __cplusplus
 }
 #endif
