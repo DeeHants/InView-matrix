e9fab94e7c26f9047a98d815e89e1570540ad351
Deanna Earley <dee@earlsoft.co.uk>
2025-12-02 01:07:06 +0000

    Adjust the pins to keep the clock and control pins together

--- pins.h
+++ pins.h
@@ -1,11 +1,11 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define PIN_STROBE 12
-#define PIN_OUTPUT 14
-#define PIN_CLOCK_Y 15
-#define PIN_DATA_Y 11
-#define PIN_CLOCK_R 10
-#define PIN_DATA_R 13
+#define PIN_STROBE 10 // 4
+#define PIN_OUTPUT 11 // 2
+#define PIN_CLOCK_Y 12 // 1
+#define PIN_DATA_Y 14 // 5
+#define PIN_CLOCK_R 13 // 6
+#define PIN_DATA_R 15 // 3
 
 #endif // PINS_H
